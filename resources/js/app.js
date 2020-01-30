f8c1bcdcda2153b79ff4c46ee886f351fb4ee119
P Montgomery <pete.montgomery@fcat.org.uk>
2020-01-30 16:32:27 +0000

    vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,12 @@ require('./plugins/swal');
 // require('./plugins/resume.min');
 // require('jquery-easing')
 
-// window.Vue = require('vue');
+window.Vue = require('vue');
 
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
 
-Vue.use(VueRouter)
+// Vue.use(VueRouter)
 // Vue.component('paginate', require('vuejs-paginate'));
 
 import Vuetify from 'vuetify'
@@ -17,24 +17,24 @@ Vue.use(Vuetify)
 
 Vue.component('app', require('./views/App.vue').default);
 
-import App from './views/App'
-import Home from './components/Home'
+// import App from './views/App'
+// import Home from './components/Home'
 
-const router = new VueRouter({
-    mode: 'history',
-    routes: [
-        {
-            path: '/dev',
-            name: 'home',
-            component: Home
-        },
-    ],
-});
+// const router = new VueRouter({
+//     mode: 'history',
+//     routes: [
+//         {
+//             path: '/dev',
+//             name: 'home',
+//             component: Home
+//         },
+//     ],
+// });
 
 
-const app = new Vue({
-    el: '#app',
-    vuetify: new Vuetify(),
-    components: { App },
-    router,
-});
+// const app = new Vue({
+//     el: '#app',
+//     vuetify: new Vuetify(),
+//     components: { App },
+//     router,
+// });
--- resources/views/analysis/dev.blade.php
+++ resources/views/analysis/dev.blade.php
@@ -1,7 +1,106 @@
 {{-- @extends('layouts.dash') --}}
-@extends('layouts.dash')
+@extends('layouts.app')
 @section('content')
+{{-- <a href="/"><img class="img-fluid mb-5 d-block mx-auto" style="height: 145px;" src="../FCAT-LOGO.jpg" alt=""></a> --}}
+<div class="container1 text-center wallpaper mx-5 schooldashboard">
+    <div class="my-5">
+        <h1 class="display-2 mb-3">{{ucfirst($school)}} Dashboard</h1>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card border-left-teal shadow h-100 py-2">
+                    <div class="px-3">
+                        <div class="row">
+                            <div class="col-sm-5 my-auto">
+                                <span class="float-left text-teal"><i class="fas fa-users fa-fw fa-3x"></i></span>
+                            </div>
+                            <div class="col-sm-7 my-auto text-right">
+                                <small class="text-muted">pupils</small>
+                                <br>
+                                <h2 class="">{{$studentCounts19[0]->Count}}</h2>
+                            </div>
+                            <div class="col-12">
+                                <hr class="m-1">
+                                <small class="text-teal float-right">Number On Roll</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card border-left-teal shadow h-100 py-2">
+                    <div class="px-3">
+                        <div class="row">
+                            <div class="col-sm-5 my-auto">
+                                <span class="float-left text-blue"><i class="fas fa-user-plus fa-fw fa-3x"></i></span>
+                            </div>
+                            <div class="col-sm-7 my-auto text-right">
+                                <small class="text-muted">% pupils</small>
+                                <br>
+                                <h2 class="">{{round($studentCounts19[1]->percent,1) ?? ''}}</h2>
+                            </div>
+                            <div class="col-12">
+                                <hr class="m-1">
+                                <small class="text-blue float-right">Pupil Premium Status</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card border-left-teal shadow h-100 py-2">
+                    <div class="px-3">
+                        <div class="row">
+                            <div class="col-sm-5 my-auto">
+                                <span class="float-left text-warning"><i class="far fa-calendar-alt fa-fw fa-3x"></i></span>
+                            </div>
+                            <div class="col-sm-7 my-auto text-right">
+                                <small class="text-muted">% present</small>
+                                <br>
+                                <h2 class="">{{round($schoolattendance->ppresent,1) ?? ''}}</h2>
+                            </div>
+                            <div class="col-12">
+                                <hr class="m-1">
+                                <small class="text-warning float-right">Whole School Attendance</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card border-left-teal shadow h-100 py-2">
+                    <div class="px-3">
+                        <div class="row">
+                            <div class="col-sm-5 my-auto">
+                                <span class="float-left text-danger"><i class="fas fa-exclamation-triangle fa-fw fa-3x"></i></span>
+                            </div>
+                            <div class="col-sm-7 my-auto text-right">
+                                <small class="text-muted">% pupils</small>
+                                <br>
+                                <h2 class="">{{round($pagroups[0]->pAt_Risk,1) ?? ''}}</h2>
+                            </div>
+                            <div class="col-12">
+                                <hr class="m-1">
+                                <small class="text-danger float-right">At risk of Persistent Absence</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-    <app></app>
+        @include('analysis/row2')
+        @include('analysis/row1')
+        @include('analysis/row3')
+        @include('analysis/row4')
+
+    </div>
+
+    <div class="container1 text-center wallpaper mx-5">
+
+        {{-- @include('analysis/oldcontext') --}}
+        {{-- @include('analysis/oldattendance') --}}
+        {{-- @include('analysis/oldpa') --}}
+
+    </div>
 
 @endsection
