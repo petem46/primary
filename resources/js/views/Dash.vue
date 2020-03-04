fc15bf0ad4a231ceca0635979faf023888a35ff7
Pete Montgomery (Work) <pete.montgomery@fcat.org.uk>
2020-03-04 16:23:36 +0000

    quick commit

--- resources/js/views/Dash.vue
+++ resources/js/views/Dash.vue
@@ -5,18 +5,14 @@
         <h1>WELCOME , </h1>
         <h1>CURRENT SCHOOL IS {{ schoolname }}</h1>
         <h1>CHOOSE A SCHOOL FROM THE NAV BAR</h1>
-        <h1
-        @click="randomAvatar"
-        >OR CHOOSE A DASHBOARD VIEW FROM THE SIDE BAR</h1>
-                <v-avatar
-            size="64"
+        <h1 @click="randomAvatar">OR CHOOSE A DASHBOARD VIEW FROM THE SIDE BAR</h1>
+        <v-avatar size="64">
+          <v-img
+            :src="randomAvatar()"
+            alt="Pupil"
           >
-            <v-img
-              :src="randomAvatar()"
-              alt="Pupil"
-            >
-            </v-img>
-          </v-avatar>
+          </v-img>
+        </v-avatar>
 
       </v-container>
     </div>
@@ -27,36 +23,34 @@ import format from "date-fns/format";
 export default {
   data() {
     return {
-          avatars: [
-        'images/avatars/bee-icon.png',
-        'images/avatars/bird-icon.png',
-        'images/avatars/bulldog-icon.png',
-        'images/avatars/cat-icon.png',
-        'images/avatars/cow-icon.png',
-        'images/avatars/elephant-icon.png',
-        'images/avatars/fish-icon.png',
-        'images/avatars/monkey-icon.png',
-        'images/avatars/penguin-icon.png',
-      ],
-    }
+      avatars: [
+        "images/avatars/bee-icon.png",
+        "images/avatars/bird-icon.png",
+        "images/avatars/bulldog-icon.png",
+        "images/avatars/cat-icon.png",
+        "images/avatars/cow-icon.png",
+        "images/avatars/elephant-icon.png",
+        "images/avatars/fish-icon.png",
+        "images/avatars/monkey-icon.png",
+        "images/avatars/penguin-icon.png"
+      ]
+    };
   },
   computed: {
-    schoolname () {
+    schoolname() {
       return this.$store.getters.getschoolname;
     },
-    whodisname () {
+    whodisname() {
       return this.$store.getters.getschoolname;
-    },
+    }
   },
   watch: {
     schoolname: function() {
-      console.log('STORE SCHOOL NAME CHANGED');
+      console.log("STORE SCHOOL NAME CHANGED");
     }
   },
   mounted() {
-    if (
-      this.$store.getters.getschoolname == null
-    ) {
+    if (this.$store.getters.getschoolname == null) {
       this.$store.commit("updateSchoolName", this.whodisschool);
       this.$store.commit("setWhoDisSchool", this.whodisschool);
       this.loaded = true;
@@ -68,14 +62,13 @@ export default {
     }
   },
   methods: {
-        randomItem (items) {
-      return items[Math.floor(Math.random()*items.length)];
+    randomItem(items) {
+      return items[Math.floor(Math.random() * items.length)];
     },
     randomAvatar() {
       console.log("AVATAR = " + '"' + this.randomItem(this.avatars) + '"');
       return this.randomItem(this.avatars);
-  },
-  },
+    }
+  }
 };
-
 </script>
